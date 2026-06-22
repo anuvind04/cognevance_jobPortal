59de75475a72f2c0c2070811e59f3daa630c9b5c
anuvind04 <panuvind04@gmail.com>
2026-06-22 18:03:35 +0530

    Point frontend to live backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://127.0.0.1:8000/api" });
-
+const API = axios.create({
+  baseURL: "https://cognevance-jobportal.onrender.com/api",
+});
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("access");
   if (token) req.headers.Authorization = `Bearer ${token}`;
